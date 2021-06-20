9e6263ec2bf7958cbd8495b85e7b8664d2675b67
mohmed2000 <noza25588@gmail.com>
2021-06-20 18:03:06 +0300

    improve-files
    fix-issues

--- scripts/main.js
+++ scripts/main.js
@@ -1,67 +1,77 @@
-let numBtn = null;
-let operatorBtn = null;
+//decrale resultEle here because i use it 12 before document ready
 let resultEle = null;
-let clearBtn = null;
-let equalBtn = null;
-(num1 = null), (operator = null), (num2 = null);
-document.addEventListener("DOMContentLoaded", function (event) {
-  numBtn = document.getElementsByClassName("btn-num");
-  operatorBtn = document.getElementsByClassName("btn-operator");
+let num1 = "";
+let operator = null;
+let num2 = "";
+
+function onBtnClick(e) {
+  let number = e.target.innerHTML;
+  if (operator === null) {
+    num1 += number;
+    resultEle.innerHTML = num1;
+  } else {
+    num2 += number;
+    resultEle.innerHTML = num2;
+  }
+}
+
+function OnOperatorClick(e) {
+  operator = e.target.innerHTML;
+  resultEle.innerHTML = "";
+}
+
+function onEqualClick() {
+  switch (operator) {
+    case "%":
+      resultEle.innerHTML = Number(num1) % Number(num2);
+      break;
+    case "/":
+      resultEle.innerHTML = Number(num1) / Number(num2);
+      break;
+    case "*":
+      resultEle.innerHTML = Number(num1) * Number(num2);
+      break;
+    case "-":
+      resultEle.innerHTML = Number(num1) - Number(num2);
+      break;
+    case "+":
+      resultEle.innerHTML = Number(num1) + Number(num2);
+      break;
+    default:
+      resultEle.innerHTML = "there are an error";
+      break;
+  }
+}
+
+function onClearClick() {
+  num1 = "";
+  num2 = "";
+  operator = null;
+  resultEle.innerHTML = "";
+}
+
+function onDocumentReady(CB) {
+  document.addEventListener("DOMContentLoaded", CB);
+}
+
+document.addEventListener("DOMContentLoaded", documentReady);
+function documentReady(e) {
+  let numBtn = document.getElementsByClassName("btn-num");
+  let operatorBtn = document.getElementsByClassName("btn-operator");
   resultEle = document.querySelector(".resultEle");
-  clearBtn = document.querySelector(".btn-clear");
-  equalBtn = document.querySelector(".btn-equal");
+  let clearBtn = document.querySelector(".btn-clear");
+  let equalBtn = document.querySelector(".btn-equal");
 
-  for (let i = 0; i < numBtn.length; i++) {
-    numBtn[i].addEventListener("click", typeBtn);
-    function typeBtn() {
-      if (operator === null) {
-        var numBtnHTML = numBtn[i].innerHTML;
-        var resultEleH = document.createTextNode(numBtnHTML);
-        resultEle.append(resultEleH);
-        num1 = resultEle.innerHTML;
-      } else if (operator != undefined) {
-        numBtnHTML = numBtn[i].innerHTML;
-        var resultEleH = document.createTextNode(numBtnHTML);
-        resultEle.append(resultEleH);
-        num2 = resultEle.innerHTML;
-      }
-    }
-  }
-  for (let i = 0; i < operatorBtn.length; i++) {
-    operatorBtn[i].addEventListener("click", typeOperator);
-    function typeOperator() {
-      var operatorBtnHTML = operatorBtn[i].innerHTML;
-      operator = operatorBtnHTML;
-      resultEle.innerHTML = "";
-      console.log(operator);
-    }
+  for (var i = 0; i < numBtn.length; i++) {
+    numBtn[i].addEventListener("click", onBtnClick);
   }
 
-  equalBtn.addEventListener("click", equal);
-  function equal() {
-    switch (operator) {
-      case "%":
-        resultEle.innerHTML = Number(num1) % Number(num2);
-        break;
-      case "/":
-        resultEle.innerHTML = Number(num1) / Number(num2);
-        break;
-      case "*":
-        resultEle.innerHTML = Number(num1) * Number(num2);
-        break;
-      case "-":
-        resultEle.innerHTML = Number(num1) - Number(num2);
-        break;
-      case "+":
-        resultEle.innerHTML = Number(num1) + Number(num2);
-        break;
-      default:
-        resultEle.innerHTML = "there are an error";
-        break;
-    }
+  for (var i = 0; i < operatorBtn.length; i++) {
+    operatorBtn[i].addEventListener("click", OnOperatorClick);
   }
-  clearBtn.addEventListener("click", Clear);
-  function Clear() {
-    resultEle.innerHTML = "";
-  }
-});
+
+  equalBtn.addEventListener("click", onEqualClick);
+
+  clearBtn.addEventListener("click", onClearClick);
+}
+onDocumentReady(documentReady);
